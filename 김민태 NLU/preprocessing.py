24f5b670dd9dbd6f3bae61195c86e99b1ab80194
kkimlee <40078267+kkimlee@users.noreply.github.com>
2020-12-23 20:53:55 +0900

    Update preprocessing.py

--- 김민태 NLU/preprocessing.py
+++ 김민태 NLU/preprocessing.py
@@ -2,6 +2,7 @@ import numpy as np
 import scipy.io as sio
 import scipy.io.wavfile
 from konlpy.tag import Okt
+from konlpy.tag import Kkma
 
 
 # 파일 읽어 오기
@@ -18,7 +19,34 @@ with open('dataset/transcript.v.1.4.txt', mode='r', encoding='utf-8') as file:
         else:
             break
 
+# Twitter 형태소 분석기
+okt = Okt()
 
+# 형태소 분석
+okt_morphs = list()
+okt_pos = list()
+okt_nouns = list()
+for data in scenario:
+    # 형태소 추출
+    okt_morphs.append(okt.morphs(data))
+    # 형태소 태깅
+    okt_pos.append(okt.pos(data))
+    # 명사 추출
+    okt_nouns.append(okt.nouns(data))
+    
+
+# 꼬꼬마 형태소 분석기
+kkma = Kkma()
+
+# 형태소 분석
+kkma_morphs = list()
+kkma_pos = list()
+kkma_nouns = list()
+for data in scenario:
+    # 형태소 추출
+    kkma_morphs.append(kkma.morphs(data))
+
+'''
 # 음성 파일 읽어오기
 speech_data = list()
 for file in file_name:
@@ -27,12 +55,5 @@ for file in file_name:
     _, data = sio.wavfile.read(dataset_path + file)
     
     speech_data.append(data)
-
-# 형태소 분석기
-okt = Okt()
-
-# 형태소 추출
-speech_data_morphs = list()
-for data in speech_data:
-    speech_data_morphs.append(okt.morphs(data))
+'''
 
