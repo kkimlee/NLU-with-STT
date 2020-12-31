852b6166446b0526a43a88aa2e9d1895db937601
kkimlee <40078267+kkimlee@users.noreply.github.com>
2020-12-31 21:06:28 +0900

    transformer model
    트랜스포머 모델 테스트

--- 김민태 NLU/preprocessing.py
+++ 김민태 NLU/preprocessing.py
@@ -3,65 +3,107 @@ import scipy.io as sio
 import scipy.io.wavfile
 from konlpy.tag import Okt
 from konlpy.tag import Kkma
+import tensorflow as tf
+import tensorflow_datasets as tfds
 
 
 # 파일 읽어 오기
-with open('dataset/transcript.v.1.4.txt', mode='r', encoding='utf-8') as file:
-    scenario = list()
-    file_name = list()
+with open('train.txt', mode='r', encoding='utf-8') as file:
+    sentence = list()
+    category = list()
     
     while True:
-        sentence = file.readline()
+        data = file.readline()
         
-        if sentence:
-            file_name.append(sentence.split('|')[0])
-            scenario.append(sentence.split('|')[1])
+        if data:
+            category.append(data.split('\t')[0])
+            sentence.append(data.split('\t')[1])
         else:
             break
+print('데이터 개수 : ', len(sentence))
 
-'''
+tokenizer = tfds.features.text.SubwordTextEncoder.build_from_corpus(category + sentence, target_vocab_size=2**13)
+# 시작 토큰과 종료 토큰에 대한 정수 부여.
+START_TOKEN, END_TOKEN = [tokenizer.vocab_size], [tokenizer.vocab_size + 1]
 
-'''
+# 시작 토큰과 종료 토큰을 고려하여 단어 집합의 크기를 + 2
+VOCAB_SIZE = tokenizer.vocab_size + 2
 
-# Twitter 형태소 분석기
-okt = Okt()
+print('시작 토큰 번호 :',START_TOKEN)
+print('종료 토큰 번호 :',END_TOKEN)
+print('단어 집합의 크기 :',VOCAB_SIZE)
 
-# 형태소 분석
-okt_morphs = list()
-okt_pos = list()
-okt_nouns = list()
-for data in scenario:
-    # 형태소 추출
-    okt_morphs.append(okt.morphs(data))
-    # 형태소 태깅
-    okt_pos.append(okt.pos(data))
-    # 명사 추출
-    okt_nouns.append(okt.nouns(data))
-    
+print('임의의 질문 샘플을 정수 인코딩 : {}'.format(tokenizer.encode(sentence[20])))
 
-# 꼬꼬마 형태소 분석기
-kkma = Kkma()
+# 서브워드텍스트인코더 토크나이저의 .encode()와 .decode() 테스트해보기
+# 임의의 입력 문장을 sample_string에 저장
+sample_string = sentence[20]
 
-# 형태소 분석
-kkma_morphs = list()
-kkma_pos = list()
-kkma_nouns = list()
-for data in scenario:
-    # 형태소 추출
-    kkma_morphs.append(kkma.morphs(data))
-    # 형태소 태깅
-    kkma_pos.append(kkma.pos(data))
-    # 명사 추출
-    kkma_nouns.append(kkma.nouns(data))
+# encode() : 텍스트 시퀀스 --> 정수 시퀀스
+tokenized_string = tokenizer.encode(sample_string)
+print ('정수 인코딩 후의 문장 {}'.format(tokenized_string))
 
-'''
-# 음성 파일 읽어오기
-speech_data = list()
-for file in file_name:
-    print(file)
-    dataset_path = 'dataset/kss/'
-    _, data = sio.wavfile.read(dataset_path + file)
-    
-    speech_data.append(data)
-'''
+# decode() : 정수 시퀀스 --> 텍스트 시퀀스
+original_string = tokenizer.decode(tokenized_string)
+print ('기존 문장: {}'.format(original_string))
 
+for ts in tokenized_string:
+  print ('{} ----> {}'.format(ts, tokenizer.decode([ts])))
+  
+# 최대 길이를 40으로 정의
+MAX_LENGTH = 40
+
+# 토큰화 / 정수 인코딩 / 시작 토큰과 종료 토큰 추가 / 패딩
+def tokenize_and_filter(inputs, outputs):
+  tokenized_inputs, tokenized_outputs = [], []
+
+  for (sentence1, sentence2) in zip(inputs, outputs):
+    # encode(토큰화 + 정수 인코딩), 시작 토큰과 종료 토큰 추가
+    sentence1 = START_TOKEN + tokenizer.encode(sentence1) + END_TOKEN
+    sentence2 = START_TOKEN + tokenizer.encode(sentence2) + END_TOKEN
+
+    tokenized_inputs.append(sentence1)
+    tokenized_outputs.append(sentence2)
+
+  # 패딩
+  tokenized_inputs = tf.keras.preprocessing.sequence.pad_sequences(
+      tokenized_inputs, maxlen=MAX_LENGTH, padding='post')
+  tokenized_outputs = tf.keras.preprocessing.sequence.pad_sequences(
+      tokenized_outputs, maxlen=MAX_LENGTH, padding='post')
+
+  return tokenized_inputs, tokenized_outputs
+
+sentence, category = tokenize_and_filter(sentence, category)
+
+print('질문 데이터의 크기(shape) :', sentence.shape)
+print('답변 데이터의 크기(shape) :', category.shape)
+
+# 0번 샘플을 임의로 출력
+print(sentence[0])
+print(category[0])
+
+# 텐서플로우 dataset을 이용하여 셔플(shuffle)을 수행하되, 배치 크기로 데이터를 묶는다.
+# 또한 이 과정에서 교사 강요(teacher forcing)을 사용하기 위해서 디코더의 입력과 실제값 시퀀스를 구성한다.
+BATCH_SIZE = 64
+BUFFER_SIZE = 20000
+
+# 디코더의 실제값 시퀀스에서는 시작 토큰을 제거해야 한다.
+dataset = tf.data.Dataset.from_tensor_slices((
+    {
+        'inputs': sentence,
+        'dec_inputs': category[:, :-1] # 디코더의 입력. 마지막 패딩 토큰이 제거된다.
+    },
+    {
+        'outputs': category[:, 1:]  # 맨 처음 토큰이 제거된다. 다시 말해 시작 토큰이 제거된다.
+    },
+))
+
+dataset = dataset.cache()
+dataset = dataset.shuffle(BUFFER_SIZE)
+dataset = dataset.batch(BATCH_SIZE)
+dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
+
+# 임의의 샘플에 대해서 [:, :-1]과 [:, 1:]이 어떤 의미를 가지는지 테스트해본다.
+print(category[0]) # 기존 샘플
+print(category[:1][:, :-1]) # 마지막 패딩 토큰 제거하면서 길이가 39가 된다.
+print(category[:1][:, 1:]) # 맨 처음 토큰이 제거된다. 다시 말해 시작 토큰이 제거된다. 길이는 역시 39가 된다.
